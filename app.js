f302fd76b19110b4b36b00674b837c4dcebd486f
aeez <makramfais@gmail.com>
2022-12-20 10:17:07 +0700

    make environment variable

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
@@ -24,8 +26,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // encrypt
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 // MODEL USER
 const User = new mongoose.model("User", userSchema);
